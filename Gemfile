fb74fc7a856ef356ca4380548068ef1d7a212f38
keyhit <viacheslavkarpenkoju@gmail.com>
2019-04-21 22:54:07 +0300

    Work at grid

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'pry'
 gem 'carrierwave', '~> 1.0'
 gem 'sendgrid-ruby'
 gem 'colorize'
-
+ 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
 # Use sqlite3 as the database for Active Record
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+  # p '--------------------- current_user -------------------'
+  # p 
+  # p '--------------------- / current_user -------------------'
+
 end
