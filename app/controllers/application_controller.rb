1674439a2bfcc7fdf8a592f4cc21ea3d6758bd87
keyhit <viacheslavkarpenkoju@gmail.com>
2019-04-26 16:12:39 +0300

    Refactoring navs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
 
+  def rest
+    @rest = "ApplicationController.rest"
+    current_user.id
+  end
+  helper_method :rest
+
+  def current_user_job
+    p 'current_user_job'
+  end
+  helper_method :current_user_job
+
   private
 
   def require_login
